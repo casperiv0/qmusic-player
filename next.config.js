5d1ec30a0386f15ee6e3aced4973af212cdb36ae
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-07-04 10:02:42 +0200

    :hammer: chore: bump deps

--- next.config.js
+++ next.config.js
@@ -6,17 +6,4 @@ module.exports = {
   images: {
     domains: ["static1.qmusic.medialaancdn.be"],
   },
-  // webpack: (config, { dev, isServer }) => {
-  //   if (!dev && !isServer) {
-  //     config.resolve.alias = {
-  //       ...config.resolve.alias,
-  //       "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-  //       react: "preact/compat",
-  //       "react-dom/test-utils": "preact/test-utils",
-  //       "react-dom": "preact/compat",
-  //     };
-  //   }
-
-  //   return config;
-  // },
 };
