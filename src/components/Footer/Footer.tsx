e08235551e77bae977cc00117c0e0531051e3e35
Casper <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2023-08-04 11:27:52 +0200

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer className={styles.footer}>
       <p>
         Not affiliated with DPG Media B.V. Created by{" "}
-        <a target="_blank" rel="noopener noreferrer" href="https://caspertheghost.me">
+        <a target="_blank" rel="noopener noreferrer" href="https://casperiv.dev">
           Dev-CasperTheGhost
         </a>
       </p>
